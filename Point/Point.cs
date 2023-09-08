2de9634653b536954d6ca561bb940b13eb91d2b1
CatsArmy <bergman.itai@gmail.com>
2023-09-08 10:26:37 +0300

    Add project files.

---
+++ Point/Point.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    public class Point
+    {
+        public double x { get; private set; }
+        public double y { get; private set; }
+        int boardSize = 10;
+
+        public Point(bool addFloat = false)
+        {
+            Random random = new Random();
+            x = random.Next(-boardSize, boardSize) + (addFloat ? random.NextDouble() : 0.0);
+            y = random.Next(-boardSize, boardSize) + (addFloat ? random.NextDouble() : 0.0);
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+        }
+        public bool IsLeft(Point p, bool sameY = false)
+        {
+            return x < p.x && y == p.y || !sameY;
+        }
+        public bool IsRight(Point p, bool sameY = false)
+        {
+            return x > p.x && y == p.y || !sameY;
+        }
+        public bool IsAbove(Point p, bool sameX = false)
+        {
+            return y > p.y && x == p.x || !sameX;
+        }
+        public bool IsBelow(Point p, bool sameX = false)
+        {
+            return y < p.y && x == p.x || !sameX;
+        }
+        public int Quardrant()
+        {
+            if (x < boardSize / 2 && y > boardSize / 2) return 1;
+            if (x > boardSize / 2 && y > boardSize / 2) return 2;
+            if (x < boardSize / 2 && y < boardSize / 2) return 3;
+            if (x > boardSize / 2 && y < boardSize / 2) return 4;
+            return 4;
+        }
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+        public bool Equals(Point p)
+        {
+            return x == p.x && y == p.y;
+        }
+        public double Distance(Point p)
+        {
+            return Math.Sqrt(Math.Pow(p.y - y, 2) + Math.Pow(p.x - x, 2));
+        }
+        public Point Mirror()
+        {
+            return new Point(x * -1, y * -1);
+        }
+        public void Move(double dx, double dy)
+        {
+            x += dx;
+            y += dy;
+        }
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,27 @@
+﻿using Points;
+
+Point p1 = new Point();
+Point p2 = new Point();
+Point p3 = new Point();
+Console.WriteLine($"p1[{p1}], p2[{p2}], p3[{p3}]");
+Console.WriteLine(HighestPoint(p1, p2, p3));
+Console.WriteLine(EqualPoints(p1, p2, p3) ? "There Are 2 Equal Points" : "There Are No Equal Points");
+Point p4 = new Point(p1);
+p4.Move(1.5, 2.5);
+Console.WriteLine(p4);
+
+bool EqualPoints(Point p1, Point p2, Point p3)
+{
+    return p1.Equals(p2) || p2.Equals(p3) || p1.Equals(p3);
+}
+
+Point HighestPoint(Point p1, Point p2, Point p3)
+{
+    return p1.IsAbove(p2) ? p1.IsAbove(p3) ? p1 : p3 : p2.IsAbove(p3) ? p2 : p3;
+}
+
+bool SameXposOrYpos(Point p1, Point p2, Point p3)
+{
+    return p1.x == p2.x && p3.x == p1.x && p2.x == p3.x ||
+        p1.y == p2.y && p3.y == p1.y && p2.y == p3.y;
+}
